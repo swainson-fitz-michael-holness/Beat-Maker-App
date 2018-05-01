248d6a9cabf076c31609def8deceaf86dcd0fd5e
exogenist <swainson.holness@gmail.com>
2018-05-01 13:53:27 -0600

    refactor js

--- main.js
+++ main.js
@@ -1,269 +1,135 @@
-//###########################
-//###(:)#####':v:`####[`'`']#
-//###|:|#####(o:0)#####|::|##
-//###|:|######(:)######|::|##
-//###########################
-
-//  _____  _____   __ _  ___ _ __ (_)___| |_
-// / _ \ \/ / _ \ / _` |/ _ \ '_ \| / __| __|
-//|  __/>  < (_) | (_| |  __/ | | | \__ \ |_
-// \___/_/\_\___/ \__, |\___|_| |_|_|___/\__|
-//                |___/
-// Swainson Holness
-
-let kickArr = [];
-let snareArr = [];
-let hihatArr = [];
-let state = [true, false, false] // state[0] = kick, state[1] = snare, state[2] = hihat,
-
-
-function checkState() {
-    for (var i = 0; i < state.length; i++) {
-        if (state[0]) {
-            return kickArr;
-        }
-        if (state[1]) {
-            return snareArr;
-        }
-        if (state[2]) {
-            return hihatArr;
-        }
-    }
-}
-
-function getNote() {
-    for (var i = 0; i < state.length; i++) {
-        if (state[0]) {
-            return "C3";
-        }
-        if (state[1]) {
-            return "D3";
-        }
-        if (state[2]) {
-            return "E3";
-        }
-    }
-}
-
-
-function getColor() {
-    for (var i = 0; i < state.length; i++) {
-        if (state[0]) {
-            return "brown";
-        }
-        if (state[1]) {
-            return "gold";
-        }
-        if (state[2]) {
-            return "lightGreen";
-        }
-    }
-}
-
-// Runs when the page loads.
-let drum = new Tone.Sampler({
-    "C3": "./sounds/kick.mp3",
-    "D3": "./sounds/snare.mp3",
-    "E3": "./sounds/hihat.mp3",
-}, () => {}).toMaster();
-//--X
-
-//=====================================
-// Dom design stuff and UI interface
-//=====================================
-for (let x = 0; x < 16; x++) {
-    $(".grid-container").append("<div class='grid-item'>" + x + "</div>");
-};
-
-let gridArr = document.querySelectorAll(".grid-item");
-
-function updateSequence(sequenceArr, val) {
-    if (sequenceArr.includes(val.innerHTML)) {
-        sequenceArr.splice(sequenceArr.findIndex((el) => {
-            return el === val.innerHTML
-        }), 1);
-        $(gridArr[val.innerHTML]).css("border-color", "black");
-    } else {
-        sequenceArr.push(val.innerHTML);
-        $(gridArr[val.innerHTML]).css("border-color", getColor());
-        drum.triggerAttackRelease(getNote());
-    }
-    sequenceArr.sort(function (a, b) {
-        return a - b
-    });
-}
-
-for (let x = 0; x < 16; x++) {
-    $(gridArr[x]).on("mousedown", () => {
-        updateSequence(checkState(), gridArr[x]);
-    });
-}
-//--X
-
-//=====================================
-// Create instrument parts for drums snare and hihat.
-//=====================================
-let kickPart = new Tone.Part((time, event) => {
-    drum.triggerAttackRelease(event.note, event.dur, time);
-}, {});
-kickPart.loop = Infinity;
-kickPart.loopEnd = '1m';
-kickPart.start(0);
-
-let snarePart = new Tone.Part((time, event) => {
-    drum.triggerAttackRelease(event.note, event.dur, time);
-}, {});
-snarePart.loop = Infinity;
-snarePart.loopEnd = '1m';
-snarePart.start(0);
-
-let hihatPart = new Tone.Part((time, event) => {
-    drum.triggerAttackRelease(event.note, event.dur, time);
-}, {});
-hihatPart.loop = Infinity;
-hihatPart.loopEnd = '1m';
-hihatPart.start(0);
-//--X
-
-
-//=====================================
-// system logic flow.
-//=====================================
-function setup(time, note, dur) { // returns object to add to part
-    return {
-        time: time,
-        note: note,
-        dur: dur
-    }
-}
-
-function parseTime(num) { // refactor this
-    if (num === "0") {
-        return 0;
-    }
-    if (num === "1") {
-        return "16n";
-    }
-    if (num > 1) {
-        let temp = "16n";
-        for (var i = 0; i < num - 1; i++) {
-            temp = temp + "+16n"
-        }
-        return temp;
-    }
-}
-
-function parsePart() {
-    if (checkState() === kickArr) {
-        return kickPart;
-    }
-    if (checkState() === snareArr) {
-        return snarePart;
-    }
-    if (checkState() === hihatArr) {
-        return hihatPart;
-    }
-}
-
-function setInstrumentState(val) {
-    $(".grid-item").off("mousedown");
-    state = val;
-    parsePart().removeAll();
-
-    //    console.log(checkState());
-
+window.onload = function () {
+    //display pads. The numbers in the pad are used to identify it.
     for (let x = 0; x < 16; x++) {
-        //        console.log(x == gridArr[x].innerHTML)
+        $(".grid-container").append("<div class='grid-item'>" + x + "</div>");
+    };
 
-        if (checkState().includes(gridArr[x].innerHTML)) {
-            $(gridArr[x]).css("border-color", getColor());
+    let gridArr = document.querySelectorAll(".grid-item");
+    let state = {
+        instrument: "kick",
+        kick: [],
+        snare: [],
+        hihat: []
+    };
+    $($("#kickClr")).css("fill", "brown");
+    //load sounds
+    let drum = new Tone.Sampler({
+        "C3": "./sounds/kick.mp3",
+        "D3": "./sounds/snare.mp3",
+        "E3": "./sounds/hihat.mp3",
+    }, () => {}).toMaster();
+
+    let kickPart = new Tone.Part((time, event) => {
+        drum.triggerAttackRelease(event.note, event.dur, time);
+    }, {});
+    kickPart.loop = Infinity;
+    kickPart.loopEnd = '1m';
+    kickPart.start(0);
+    //------------
+
+
+    //Cosmetically switches state
+    function loadState(val, color) {
+        for (let x = 0; x < 16; x++) {
+            if (val.includes(gridArr[x].innerHTML)) {
+                $(gridArr[x]).css("border-color", color);
+            } else {
+                $(gridArr[x]).css("border-color", "black");
+            }
+        }
+    }
+
+    //instrument states
+    function stateBtn() {
+        $("#kick").on("click", (e) => {
+            state.instrument = "kick";
+            $($("#kickClr")).css("fill", "brown");
+            $($(".snareClr")).attr("fill", "#C5EFE5");
+            $($(".hihatClr")).attr("fill", "#C5EFE5");
+            loadState(state.kick, "brown");
+        });
+        $("#snare").on("click", (e) => {
+            state.instrument = "snare";
+            $($("#kickClr")).css("fill", "#C5EFE5");
+            $($(".snareClr")).attr("fill", "gold");
+            $($(".hihatClr")).attr("fill", "#C5EFE5");
+            loadState(state.snare, "gold");
+        });
+        $("#hihat").on("click", (e) => {
+            state.instrument = "hihat";
+            $($("#kickClr")).css("fill", "#C5EFE5");
+            $($(".snareClr")).attr("fill", "#C5EFE5");
+            $($(".hihatClr")).attr("fill", "lightGreen");
+            loadState(state.hihat, "lightGreen");
+        });
+    }
+    stateBtn();
+
+    //turns on pad buttons
+    for (let x = 0; x < 16; x++) {
+        $(gridArr[x]).on("mousedown", () => {
+            //check the state of the button
+            if ($(gridArr[x]).css("border-color") == "rgb(0, 0, 0)") {
+                //TURN ON
+                if (state.instrument == "kick") {
+                    $(gridArr[x]).css("border-color", "brown");
+                    state.kick.push(gridArr[x].innerHTML);
+
+                } else if (state.instrument == "snare") {
+                    $(gridArr[x]).css("border-color", "gold");
+                    state.snare.push(gridArr[x].innerHTML);
+
+                } else if (state.instrument == "hihat") {
+                    $(gridArr[x]).css("border-color", "lightGreen");
+                    state.hihat.push(gridArr[x].innerHTML);
+
+                }
+            } else {
+                //TURN OFF
+                if (state.instrument == "kick") {
+                    $(gridArr[x]).css("border-color", "rgb(0, 0, 0)");
+                    state.kick.splice(state.kick.indexOf(x.toString()), 1);
+
+                } else if (state.instrument == "snare") {
+                    $(gridArr[x]).css("border-color", "rgb(0, 0, 0)");
+                    state.snare.splice(state.snare.indexOf(x.toString()), 1);
+
+                } else if (state.instrument == "hihat") {
+                    $(gridArr[x]).css("border-color", "rgb(0, 0, 0)");
+                    state.hihat.splice(state.hihat.indexOf(x.toString()), 1);
+
+                }
+            }
+
+        });
+    }
+    $("#play").on("click", (e) => {
+        e.preventDefault;
+        parser(addParts);
+        if (checkState().length === 0) {
+
         } else {
-            $(gridArr[x]).css("border-color", "black");
+            //        $(".grid-item").off("mousedown");
+            //        $("#kick").off("click");
+            //        $("#snare").off("click");
+            //        $("#hihat").off("click");
+            stateOn = true;
+            $("#stop").attr("display", "inline");
+            //        for (let x = 0; x < kickArr.length; x++) {
+            //            parsePart().add(setup(parseTime(kickArr[x]), "C3", "4n"));
+            //        }
+            //        for (let x = 0; x < snareArr.length; x++) {
+            //            parsePart().add(setup(parseTime(snareArr[x]), "D3", "4n"));
+            //        }
+            //        for (let x = 0; x < hihatArr.length; x++) {
+            //            parsePart().add(setup(parseTime(hihatArr[x]), "E3", "4n"));
+            //        }
 
+            Tone.Transport.start('+0.1');
         }
+    });
 
-        $(gridArr[x]).on("mousedown", () => {
-            updateSequence(checkState(), gridArr[x]);
-            //            console.log($(gridArr[x].innerHTML));
-        });
-    }
+
 
 
 }
-
-// Button controls =====================================================
-function controlBtnOn() {
-    $("#kick").on("click", (e) => {
-        $($("#kickClr")).css("fill", "brown");
-        $($(".snareClr")).attr("fill", "#C5EFE5");
-        $($(".hihatClr")).attr("fill", "#C5EFE5");
-        setInstrumentState([true, false, false]);
-    });
-    $("#snare").on("click", (e) => {
-        $($("#kickClr")).css("fill", "#C5EFE5");
-        $($(".snareClr")).attr("fill", "gold");
-        $($(".hihatClr")).attr("fill", "#C5EFE5");
-        setInstrumentState([false, true, false]);
-    });
-    $("#hihat").on("click", (e) => {
-        $($("#kickClr")).css("fill", "#C5EFE5");
-        $($(".snareClr")).attr("fill", "#C5EFE5");
-        $($(".hihatClr")).attr("fill", "lightGreen");
-        setInstrumentState([false, false, true]);
-    });
-}
-
-$("#play").on("click", (e) => {
-    e.preventDefault;
-    if (checkState().length === 0) {
-
-    } else {
-        $(".grid-item").off("mousedown");
-        $("#kick").off("click");
-        $("#snare").off("click");
-        $("#hihat").off("click");
-
-        $("#stop").attr("display", "inline");
-        for (let x = 0; x < kickArr.length; x++) {
-            parsePart().add(setup(parseTime(kickArr[x]), "C3", "4n"));
-        }
-        for (let x = 0; x < snareArr.length; x++) {
-            parsePart().add(setup(parseTime(snareArr[x]), "D3", "4n"));
-        }
-        for (let x = 0; x < hihatArr.length; x++) {
-            parsePart().add(setup(parseTime(hihatArr[x]), "E3", "4n"));
-        }
-
-        Tone.Transport.start('+0.1');
-    }
-});
-
-$("#stop").on("click", (e) => {
-    Tone.Transport.stop();
-    $(".grid-item").off("mousedown");
-    parsePart().removeAll();
-    $("#stop").attr("display", "none");
-
-    for (let x = 0; x < 16; x++) {
-        $(gridArr[x]).on("mousedown", () => {
-            updateSequence(checkState(), gridArr[x]);
-        });
-    }
-controlBtnOn();
-});
-
-controlBtnOn();
-
-
-
-
-//                                    _
-//        ___ _   _ ___  ___ ___   __| | ___
-//  _____/ __| | | / __|/ __/ _ \ / _` |/ _ \
-// |_____\__ \ |_| \__ \ (_| (_) | (_| |  __/
-//       |___/\__, |___/\___\___/ \__,_|\___|
-//            |___/
-
-// kick drum icon created by Raz Cohen
-// snare drum icon created by Andrejs Kirma
-// hihat icon created by Dmitry Mirolyubov
---
+++ prototype.js
@@ -0,0 +1,312 @@
+//###########################
+//###(:)#####':v:`####[`'`']#
+//###|:|#####(o:0)#####|::|##
+//###|:|######(:)######|::|##
+//###########################
+
+//  _____  _____   __ _  ___ _ __ (_)___| |_
+// / _ \ \/ / _ \ / _` |/ _ \ '_ \| / __| __|
+//|  __/>  < (_) | (_| |  __/ | | | \__ \ |_
+// \___/_/\_\___/ \__, |\___|_| |_|_|___/\__|
+//                |___/
+// Swainson Holness
+
+let kickArr = [];
+let snareArr = [];
+let hihatArr = [];
+let state = [true, false, false] // state[0] = kick, state[1] = snare, state[2] = hihat,
+let stateOn = false;
+
+
+function checkState() {
+    for (var i = 0; i < state.length; i++) {
+        if (state[0]) {
+            return kickArr;
+        }
+        if (state[1]) {
+            return snareArr;
+        }
+        if (state[2]) {
+            return hihatArr;
+        }
+    }
+}
+
+function getNote() {
+    for (var i = 0; i < state.length; i++) {
+        if (state[0]) {
+            return "C3";
+        }
+        if (state[1]) {
+            return "D3";
+        }
+        if (state[2]) {
+            return "E3";
+        }
+    }
+}
+
+
+function getColor() {
+    for (var i = 0; i < state.length; i++) {
+        if (state[0]) {
+            return "brown";
+        }
+        if (state[1]) {
+            return "gold";
+        }
+        if (state[2]) {
+            return "lightGreen";
+        }
+    }
+}
+
+function addParts() {
+        for (let x = 0; x < kickArr.length; x++) {
+            parsePart().add(setup(parseTime(kickArr[x]), "C3", "4n"));
+        }
+        for (let x = 0; x < snareArr.length; x++) {
+            parsePart().add(setup(parseTime(snareArr[x]), "D3", "4n"));
+        }
+        for (let x = 0; x < hihatArr.length; x++) {
+            parsePart().add(setup(parseTime(hihatArr[x]), "E3", "4n"));
+        }
+}
+
+//parser
+function parser(init) {
+    parsePart().removeAll();
+    init();
+}
+
+// Runs when the page loads.
+let drum = new Tone.Sampler({
+    "C3": "./sounds/kick.mp3",
+    "D3": "./sounds/snare.mp3",
+    "E3": "./sounds/hihat.mp3",
+}, () => {}).toMaster();
+//--X
+
+//=====================================
+// Dom design stuff and UI interface
+//=====================================
+for (let x = 0; x < 16; x++) {
+    $(".grid-container").append("<div class='grid-item'>" + x + "</div>");
+};
+
+let gridArr = document.querySelectorAll(".grid-item");
+
+function updateSequence(sequenceArr, val) {
+    if (sequenceArr.includes(val.innerHTML)) {
+        sequenceArr.splice(sequenceArr.findIndex((el) => {
+            return el === val.innerHTML
+        }), 1);
+        $(gridArr[val.innerHTML]).css("border-color", "black");
+        parser(addParts);
+    } else {
+        sequenceArr.push(val.innerHTML);
+        $(gridArr[val.innerHTML]).css("border-color", getColor());
+        drum.triggerAttackRelease(getNote());
+        parser(addParts);
+
+    }
+    sequenceArr.sort(function (a, b) {
+        return a - b
+    });
+    console.log(parsePart());
+
+
+
+//    for (let x = 0; x < kickArr.length; x++) {
+//        parsePart().add(setup(parseTime(kickArr[x]), "C3", "4n"));
+//    }
+//    for (let x = 0; x < snareArr.length; x++) {
+//        parsePart().add(setup(parseTime(snareArr[x]), "D3", "4n"));
+//    }
+//    for (let x = 0; x < hihatArr.length; x++) {
+//        parsePart().add(setup(parseTime(hihatArr[x]), "E3", "4n"));
+//    }
+
+
+}
+
+for (let x = 0; x < 16; x++) {
+    $(gridArr[x]).on("mousedown", () => {
+        updateSequence(checkState(), gridArr[x]);
+    });
+}
+//--X
+
+//=====================================
+// Create instrument parts for drums snare and hihat.
+//=====================================
+let kickPart = new Tone.Part((time, event) => {
+    drum.triggerAttackRelease(event.note, event.dur, time);
+}, {});
+kickPart.loop = Infinity;
+kickPart.loopEnd = '1m';
+kickPart.start(0);
+
+let snarePart = new Tone.Part((time, event) => {
+    drum.triggerAttackRelease(event.note, event.dur, time);
+}, {});
+snarePart.loop = Infinity;
+snarePart.loopEnd = '1m';
+snarePart.start(0);
+
+let hihatPart = new Tone.Part((time, event) => {
+    drum.triggerAttackRelease(event.note, event.dur, time);
+}, {});
+hihatPart.loop = Infinity;
+hihatPart.loopEnd = '1m';
+hihatPart.start(0);
+//--X
+
+
+//=====================================
+// system logic flow.
+//=====================================
+function setup(time, note, dur) { // returns object to add to part
+    return {
+        time: time,
+        note: note,
+        dur: dur
+    }
+}
+
+function parseTime(num) { // refactor this
+    if (num === "0") {
+        return 0;
+    }
+    if (num === "1") {
+        return "16n";
+    }
+    if (num > 1) {
+        let temp = "16n";
+        for (var i = 0; i < num - 1; i++) {
+            temp = temp + "+16n"
+        }
+        return temp;
+    }
+}
+
+function parsePart() {
+    if (checkState() === kickArr) {
+        return kickPart;
+    }
+    if (checkState() === snareArr) {
+        return snarePart;
+    }
+    if (checkState() === hihatArr) {
+        return hihatPart;
+    }
+}
+
+function setInstrumentState(val) {
+    $(".grid-item").off("mousedown");
+    state = val;
+    //    parsePart().removeAll();
+
+    //    console.log(checkState());
+
+    for (let x = 0; x < 16; x++) {
+        //        console.log(x == gridArr[x].innerHTML)
+
+        if (checkState().includes(gridArr[x].innerHTML)) {
+            $(gridArr[x]).css("border-color", getColor());
+        } else {
+            $(gridArr[x]).css("border-color", "black");
+
+        }
+
+        $(gridArr[x]).on("mousedown", () => {
+            updateSequence(checkState(), gridArr[x]);
+            //            console.log($(gridArr[x].innerHTML));
+        });
+    }
+
+
+}
+
+// Button controls =====================================================
+function controlBtnOn() {
+    $("#kick").on("click", (e) => {
+
+        $($("#kickClr")).css("fill", "brown");
+        $($(".snareClr")).attr("fill", "#C5EFE5");
+        $($(".hihatClr")).attr("fill", "#C5EFE5");
+        setInstrumentState([true, false, false]);
+//                parser(addParts);
+    });
+    $("#snare").on("click", (e) => {
+//        parser(addParts);
+        $($("#kickClr")).css("fill", "#C5EFE5");
+        $($(".snareClr")).attr("fill", "gold");
+        $($(".hihatClr")).attr("fill", "#C5EFE5");
+        setInstrumentState([false, true, false]);
+    });
+    $("#hihat").on("click", (e) => {
+//        parser(addParts);
+        $($("#kickClr")).css("fill", "#C5EFE5");
+        $($(".snareClr")).attr("fill", "#C5EFE5");
+        $($(".hihatClr")).attr("fill", "lightGreen");
+        setInstrumentState([false, false, true]);
+    });
+}
+
+$("#play").on("click", (e) => {
+    e.preventDefault;
+    parser(addParts);
+    if (checkState().length === 0) {
+
+    } else {
+        //        $(".grid-item").off("mousedown");
+        //        $("#kick").off("click");
+        //        $("#snare").off("click");
+        //        $("#hihat").off("click");
+        stateOn = true;
+        $("#stop").attr("display", "inline");
+        //        for (let x = 0; x < kickArr.length; x++) {
+        //            parsePart().add(setup(parseTime(kickArr[x]), "C3", "4n"));
+        //        }
+        //        for (let x = 0; x < snareArr.length; x++) {
+        //            parsePart().add(setup(parseTime(snareArr[x]), "D3", "4n"));
+        //        }
+        //        for (let x = 0; x < hihatArr.length; x++) {
+        //            parsePart().add(setup(parseTime(hihatArr[x]), "E3", "4n"));
+        //        }
+
+        Tone.Transport.start('+0.1');
+    }
+});
+
+$("#stop").on("click", (e) => {
+    Tone.Transport.stop();
+    stateOn = false;
+    $(".grid-item").off("mousedown");
+    parsePart().removeAll();
+    $("#stop").attr("display", "none");
+
+    for (let x = 0; x < 16; x++) {
+        $(gridArr[x]).on("mousedown", () => {
+            updateSequence(checkState(), gridArr[x]);
+        });
+    }
+    controlBtnOn();
+});
+
+controlBtnOn();
+
+
+
+
+//                                    _
+//        ___ _   _ ___  ___ ___   __| | ___
+//  _____/ __| | | / __|/ __/ _ \ / _` |/ _ \
+// |_____\__ \ |_| \__ \ (_| (_) | (_| |  __/
+//       |___/\__, |___/\___\___/ \__,_|\___|
+//            |___/
+
+// kick drum icon created by Raz Cohen
+// snare drum icon created by Andrejs Kirma
+// hihat icon created by Dmitry Mirolyubov
